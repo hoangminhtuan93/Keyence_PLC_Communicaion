0c8b1b5d266993726d0862341dd757a3f17863ef
minhtuantdh93@gmail.com <minhtuantdh93@gmail.com>
2021-08-26 22:16:47 +0700

    open and close

---
+++ Keyence_Communication/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Keyence_Communication
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbo_port = new System.Windows.Forms.ComboBox();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.rtb_result = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port:";
+            // 
+            // cbo_port
+            // 
+            this.cbo_port.FormattingEnabled = true;
+            this.cbo_port.Location = new System.Drawing.Point(78, 28);
+            this.cbo_port.Name = "cbo_port";
+            this.cbo_port.Size = new System.Drawing.Size(160, 23);
+            this.cbo_port.TabIndex = 1;
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(78, 78);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(75, 23);
+            this.btn_open.TabIndex = 2;
+            this.btn_open.Text = "Open";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(163, 78);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(75, 23);
+            this.btn_close.TabIndex = 2;
+            this.btn_close.Text = "Close";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // rtb_result
+            // 
+            this.rtb_result.Location = new System.Drawing.Point(313, 12);
+            this.rtb_result.Name = "rtb_result";
+            this.rtb_result.Size = new System.Drawing.Size(194, 159);
+            this.rtb_result.TabIndex = 3;
+            this.rtb_result.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtb_result);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.btn_open);
+            this.Controls.Add(this.cbo_port);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Keyence Communication";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbo_port;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.RichTextBox rtb_result;
+    }
+}
+
---
+++ Keyence_Communication/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Keyence_Communication
+{
+    public partial class Form1 : Form
+    {
+        SerialPort sp_main = new SerialPort();
+        public Form1()
+        {
+            InitializeComponent();
+            sp_main.BaudRate = 9600;
+            sp_main.DataBits = 8;
+            sp_main.StopBits = StopBits.One;
+            sp_main.Parity = Parity.Even;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] arr_port_list = SerialPort.GetPortNames();
+            cbo_port.Items.Add(string.Empty);
+            foreach (string item in arr_port_list)
+            {
+                cbo_port.Items.Add(item);
+            }
+
+        }
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            sp_main.PortName = cbo_port.Text;
+            sp_main.Open();
+            //byte[] command = { 0x43, 0x52, 0x0d };
+            string command = "CR\r\n";
+            sp_main.Write(command);
+            rtb_result.Text += sp_main.ReadLine();
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            if (!sp_main.IsOpen)
+            {
+                sp_main.PortName = cbo_port.Text; 
+            }
+            string command = "CQ\r\n";
+            sp_main.Write(command);
+            rtb_result.Text += sp_main.ReadLine();
+        }
+    }
+}
